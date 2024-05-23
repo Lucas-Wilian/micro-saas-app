ae40e63af42763b6f7174562cb8cad162a219ff6
Lucas-Wilian <lucaswilianr.s@gmail.com>
2024-05-22 22:12:50 -0300

    page configurações

--- src/app/app/settings/page.tsx
+++ src/app/app/settings/page.tsx
@@ -1,7 +1,21 @@
-export default function Page() {
+import {
+  DashboardPage,
+  DashboardPageHeader,
+  DashboardPageHeaderTitle,
+  DashboardPageMain,
+} from '@/components/dashboard/page';
+
+export default async function Page() {
   return (
-    <main className='flex items-center justify-center h-screen'>
-      <h1>settings</h1>
-    </main>
+    <DashboardPage>
+      <DashboardPageHeader>
+        <DashboardPageHeaderTitle>
+          Configurações
+        </DashboardPageHeaderTitle>
+      </DashboardPageHeader>
+      <DashboardPageMain>
+        <h1>Configurações</h1>
+      </DashboardPageMain>
+    </DashboardPage>
   );
 }
