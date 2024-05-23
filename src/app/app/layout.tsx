4aa13065bb4d283972cc655ccfc4c25cfa56fa26
Lucas-Wilian <lucaswilianr.s@gmail.com>
2024-05-22 22:11:39 -0300

    user auth mainSidebar

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,10 +1,15 @@
 import React, { PropsWithChildren } from 'react';
 import { MainSidebar } from './_componentes/main-sidebar';
+import { auth } from '@/services/auth';
 
-export default function Layout({ children }: PropsWithChildren) {
+export default async function Layout({
+  children,
+}: PropsWithChildren) {
+  const session = await auth();
+
   return (
     <div className='grid grid-cols-[16rem_1fr] gap-4'>
-      <MainSidebar />
+      <MainSidebar user={session?.user} />
       <main>{children}</main>
     </div>
   );
